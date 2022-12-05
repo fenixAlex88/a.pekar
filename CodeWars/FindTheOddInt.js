d64614c29433c953993d71a95738babcbce971f4
Alex <fenix_al@tut.by>
2022-12-05 21:07:05 +0300

    1

--- CodeWars/FindTheOddInt.js
+++ CodeWars/FindTheOddInt.js
@@ -1,13 +1,6 @@
-function findOdd(a) {
-    let count = {};
-    a.forEach(el => {
-        if (!(el in count))
-            count[el] = 0;
-        count[el]++;
-    });
-    for (let key in count) {
-        if (count[key] % 2 !== 0) return parseInt(key);
-    }
+function isPythagoreanTriple(integers) {
+    integers.sort((a, b) => (a - b))
+    return integers[0]**2+integers[1]**2===integers[2]**2;
 }
-//const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
-console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+
+console.log(isPythagoreanTriple([3, 5, 4]))
