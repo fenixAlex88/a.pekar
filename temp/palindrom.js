e09a410b45bb9eb0d55c6b90c3660f2071129132
Aleksey Pekar <96794219+fenixAlex88@users.noreply.github.com>
2022-12-06 10:35:48 +0300

    Update palindrom.js

--- temp/palindrom.js
+++ temp/palindrom.js
@@ -1,11 +1,27 @@
-const str = 'sdssd  s'
 function isPalindrom (str) {
-const ex = ' .?!,';
+const ex = ' .?!,ьъ';
     for (let i =0, j=str.length-1; i<j; i++,j--){
         while (ex.includes(str[i])) i++;
         while (ex.includes(str[j])) j--;
-        if (str[i].toLowerCase()!==str[j].toLowerCase()) return false;
+        if (str[i].toLowerCase().replace('ё','е')!==str[j].toLowerCase().replace('ё','е')) {
+            console.log(str[i].replace('ё','е').toLowerCase());
+            console.log(str[j].replace('ё','е').toLowerCase());
+            return false;
+        }
     }
     return true;
 }
-console.log(isPalindrom(str));
+
+
+const isPalindromeRecurs = str => {
+    const ex = ' .?!,';
+      return str.length <= 1 ? true : 
+      ex.includes(str[0])? is_palindrome(str.slice(1)):
+      ex.includes(str[str.length-1])? is_palindrome(str.slice(0,-1)):
+      str[0].toLowerCase().replace('е','ё') === str[str.length-1].toLowerCase().replace('е','ё') ? is_palindrome(str.slice(1,-1)):
+      false;
+}
+
+console.log(isPalindrom('е!ЁЁеЕsdssdsЁёё  .,,,е.. .. ё'));
+console.log(is_palindrome('е!ЁЁеЕsdssdsЁёё  .,,,е.. .. ё'));
+
