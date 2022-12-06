4b7bc94bedd1ab7240054aaaa9345b8c6231eab0
Aleksey Pekar <96794219+fenixAlex88@users.noreply.github.com>
2022-12-06 09:24:59 +0300

    Update palindrom.js

--- temp/palindrom.js
+++ temp/palindrom.js
@@ -2,8 +2,8 @@ const str = 'sdssd  s'
 function isPalindrom (str) {
 const ex = ' .?!,';
     for (let i =0, j=str.length-1; i<j; i++,j--){
-        if (ex.includes(str[i])) i++;
-        if (ex.includes(str[j])) j--;
+        while (ex.includes(str[i])) i++;
+        while (ex.includes(str[j])) j--;
         if (str[i].toLowerCase()!==str[j].toLowerCase()) return false;
     }
     return true;
