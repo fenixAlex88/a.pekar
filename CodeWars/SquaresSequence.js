2480855aa02e5936f8799f7885c66553fed3adae
Alex <fenix_al@tut.by>
2022-12-09 23:23:13 +0300

    added roots

--- CodeWars/SquaresSequence.js
+++ CodeWars/SquaresSequence.js
@@ -1,10 +1,10 @@
 function squares(x, n) {
     let arr = [];
-    for (let i = 0; i<n;i++){
-        arr.push(x);
-        x=x*x;
+    for (let i = 0; i < n; i++) {
+        arr.push(x ** 2 ** i);
     }
     return arr;
 }
 
-console.log(squares(5,3));
+//const squares = (x, n) => {const arr = Array(n); return arr.map((e,i)=>x ** 2 ** i);}
+console.log(squares(5, 3));
