7e1229f6baecf2240ac6539430620429c00e035d
unknown <fenix_al@tut.by>
2023-01-28 10:13:29 +0300

    added tennis

--- 50TennisCanvas/app.js
+++ 50TennisCanvas/app.js
@@ -1,17 +1,19 @@
 const canvas = document.getElementById('app');
 const ctx = canvas.getContext('2d');
-
-const game = (y) => {
+let y = 250;
+let ballx = 200;
+let bally = 200;
+const game = () => {
     canvas.style.cursor = 'none';
-    if (y>500) y=500;
-    if (y<0) y=0;
+    if (y > 500) y = 500;
+    if (y < 0) y = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#eeec7e';
     ctx.fillRect(0, 0, 800, 600)
     ctx.fill();
     ctx.fillStyle = "rgb(238,33,56)";
     ctx.beginPath();
-    ctx.arc(500, 200, 20, 0, Math.PI * 2);
+    ctx.arc(250, 250, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.fillStyle = '#191497';
@@ -19,9 +21,8 @@ const game = (y) => {
     ctx.fillRect(780, y, 20, 100);
     ctx.closePath();
     ctx.fill();
-    console.log(y)
 }
 
-canvas.addEventListener('mousemove', (e)=>{
-    game(e.clientY-50)
-})
+canvas.addEventListener('mousemove', (e) => y = e.clientY - 50);
+
+setInterval(game,30)
