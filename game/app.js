3dd7209ac83fc5fb3f82ada7769b0ecf699a354e
unknown <fenix_al@tut.by>
2023-02-01 01:23:37 +0300

    added game

--- game/app.js
+++ game/app.js
@@ -1,5 +1,4 @@
 //СОЗДАНИЕ МИРА
-
 const canvas = document.querySelector('#render-canvas');
 
 //Создание движка
@@ -15,9 +14,30 @@ scene.createDefaultEnvironment({
     cameraContrast: 2.5,
     cameraExposure: 1
 });
+//Создание неба
+const skybox = new BABYLON.MeshBuilder.CreateBox('skyBox', {size: 1000}, scene);
+const skyboxMateerial = new BABYLON.StandardMaterial('skyBox', scene);
+skyboxMateerial.reflectionTexture = new BABYLON.CubeTexture('assets/environment/TropicalSunnyDay', scene);
+skyboxMateerial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+skyboxMateerial.backFaceCulling = false;
+skybox.material = skyboxMateerial;
+//Создание моря
+const waterGround = new BABYLON.MeshBuilder.CreateGround('ground', {width: 512, height: 512}, scene);
+const waterMaterial = new BABYLON.WaterMaterial('water', scene);
+waterMaterial.bumpTexture = new BABYLON.Texture('assets/environment/waterbump.png', scene);
+waterMaterial.addToRenderList(skybox);
+waterGround.material = waterMaterial;
+waterGround.position = new BABYLON.Vector3(0, -3, 0);
+//создание дна
+const ground = new BABYLON.MeshBuilder.CreateGround('ground', {width: 128, height: 128}, scene);
+const groundmaterial = new BABYLON.StandardMaterial('material', scene);
+groundmaterial.emissiveTexture = new BABYLON.Texture('assets/environment/ground.jpg');
+ground.material = groundmaterial;
+ground.position = new BABYLON.Vector3(0, -5, 13);
+waterMaterial.addToRenderList(ground);
 
 //Создание камеры
-const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(6, 3, -10), scene);
+const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 8, -14), scene);
 camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 
 //Создание света
@@ -30,6 +50,7 @@ const shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
 //СОЗДАНИЕ ПРЕДМЕТОВ
 const boxArr = [];
 const coinArr = [];
+const pointArr = [];
 
 
 //Создание платформы и материала
@@ -73,18 +94,6 @@ const createCoin = (pos) => {
     );
 }
 
-BABYLON.SceneLoader.ImportMesh(
-    null,
-    'assets/mustang/',
-    'scene.gltf',
-    scene,
-    (meshArray) => {
-        const car = meshArray[0];
-        car.position = new BABYLON.Vector3(0, 0, 0);
-        shadowGenerator.addShadowCaster(car);
-        car.receiveShadows = true;
-    }
-);
 
 //Создание препятствий
 //let lastRand = 0;
@@ -112,7 +121,10 @@ const createRowBox = (zPos) => {
      lastRand = rand;*/
     for (let i = 0; i < 3; i++) {
         if (i === rand) {
-            createCoin(new BABYLON.Vector3(2 * i - 2, 1, zPos + 3))
+            if (Math.random() < 0.5) {
+                createCoin(new BABYLON.Vector3(2 * i - 2, 1, zPos + 3));
+            }
+            pointArr.push(new BABYLON.Vector3(2 * i - 2, 1, zPos + 3));
             continue;
         }
         createBox(2 * i - 2, zPos)
@@ -138,23 +150,84 @@ ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor
 
 
 //АЛГОРИТМ
+//Теги
+const scoreInfo = document.querySelector('#score');
+const coinScoreInfo = document.querySelector('#coin-score');
 const restartBtn = document.querySelector('#restart-btn');
+const playScreen = document.querySelector('#play-screen');
+const gameOverScreen = document.querySelector('#game-over-screen');
+const bestScoreInfo = document.querySelector('#best-score');
+const nowScoreInfo = document.querySelector('#now-score');
+
+//Переменные
+let score = 0;
+let coinScore = 0;
+let state = 'play';
+
+//функции
+const saveCoinScore = () => {
+    localStorage.coinScore = coinScore;
+}
+const loadCoinScore = () => {
+    coinScore = localStorage.coinScore ? localStorage.coinScore : 0;
+    coinScoreInfo.textContent = coinScore;
+}
+loadCoinScore();
+
+const saveBestScore = () => {
+    localStorage.bestScore = score;
+}
+
+const loadBestScore = () => localStorage.bestScore ? localStorage.bestScore : 0;
+
+
+const setGameOverScreen = () => {
+    playScreen.style.display = 'none';
+    gameOverScreen.style.display = 'block';
+    let bestScore = loadBestScore();
+    if (score > bestScore) {
+        saveBestScore();
+        bestScore = score;
+        bestScoreInfo.style.color = '#0003ad';
+        nowScoreInfo.style.color = '#0003ad';
+    }
+    bestScoreInfo.textContent = `BEST: ${bestScore}`;
+    nowScoreInfo.textContent = `NOW: ${score}`;
+}
+
+//встроенные функции Babylon
 scene.registerBeforeRender(() => {
     boxArr.forEach((box) => {
         if (ball.intersectsMesh(box, true)) {
             box.material.emissiveColor = new BABYLON.Color3(0.5, 0, 0);
+            setGameOverScreen();
+            state = 'game over';
         }
     })
-    coinArr.forEach((coin) => {
+    coinArr.forEach((coin, index) => {
         if (ball.intersectsMesh(coin, false)) {
+            coinScore++;
+            coinScoreInfo.textContent = coinScore;
+            saveCoinScore();
             coin.dispose();
+            coinArr.splice(index, 1);
         }
     })
+    pointArr.forEach((point, index) => {
+        if (ball.intersectsPoint(point)) {
+            pointArr.splice(index, 1);
+            score++;
+            scoreInfo.textContent = score;
+        }
+    })
+    skybox.position.z = ball.getAbsolutePosition().z;
+    waterGround.z = ball.getAbsolutePosition().z;
+    ground.z = ball.getAbsolutePosition().z + 13;
+    camera.position.z = ball.getAbsolutePosition().z - 12;
+    light.position.z = ball.getAbsolutePosition().z;
 });
 
 engine.runRenderLoop(() => {
-    camera.position.z = ball.getAbsolutePosition().z - 12;
-    light.position.z = ball.getAbsolutePosition().z;
     scene.render();
 
 })
@@ -163,14 +236,14 @@ engine.runRenderLoop(() => {
 restartBtn.addEventListener('click', () => window.location.reload());
 
 window.addEventListener('touchstart', (e) => {
-    const x = e.touches[0].screenX;
-    console.log(x);
-    if (x > (window.screen.width / 2)) {
-        ball.physicsImpostor.applyImpulse(new BABYLON.Vector3(5, 0, 0), ball.getAbsolutePosition())
-    } else {
-        ball.physicsImpostor.applyImpulse(new BABYLON.Vector3(-5, 0, 0), ball.getAbsolutePosition())
-    }
-
+    if (state !== 'game over') {
+        const x = e.touches[0].screenX;
+        if (x > (window.screen.width / 2)) {
+            ball.physicsImpostor.applyImpulse(new BABYLON.Vector3(5, 0, 0), ball.getAbsolutePosition())
+        } else {
+            ball.physicsImpostor.applyImpulse(new BABYLON.Vector3(-5, 0, 0), ball.getAbsolutePosition())
+        }
+    } else window.location.reload();
 });
 
 window.addEventListener('touchend', () => {
